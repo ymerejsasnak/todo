424a5de8790a38462ee2e83eb37c9bbf9950f2f1
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-10-03 19:56:37 -0400

    can move from todo to done list and delete from done list

--- README.md
+++ README.md
@@ -5,9 +5,8 @@ To Do List (easy JS practice) :)
 plan:
 no libraries!
 user can type items which get added to list on left
-user can click on item on left to send it to 'completed' list on right (or make them draggable?)
-add a way to delete items from any list
-make some kind of heirarchy/multiple to do lists for short term/long term stuff?  or at least ability to reorder items
+user can click on item on left to send it to 'completed' list on right 
+clicking on item in done list deletes it
 save it using localstorage (have naming option to save/load multiple lists and clear list too)
 
 anything else to add to make it a bit more interesting?
--- todo.js
+++ todo.js
@@ -2,28 +2,47 @@
     var elements = {toAddItem: document.getElementById('to-add-item'),
                     addItem:  document.getElementById('add-item'),
                     toDoList: document.getElementById('todo-list'),
+                    doneList: document.getElementById('done-list')
                     };
 
     
     function addItem(list, item) {
         var itemNode = document.createElement('li');
         var textNode = document.createTextNode(item);
+        
         itemNode.appendChild(textNode);
-        list.appendChild(itemNode);
+        list.appendChild(itemNode);        
     }
-
 
     
 
+
+    
+    //add an element after hitting 'add' button
     elements.addItem.addEventListener('click', function() {
         addItem(elements.toDoList, elements.toAddItem.value);
     });
-    
 
 
+    //remove an element from to do list if clicked on and move it to done list
+    elements.toDoList.addEventListener('click', function(event) {
+        var itemNode = event.target;
+
+        if (itemNode.tagName.toLowerCase() === 'li') {
+            addItem(elements.doneList, itemNode.innerText)
+            itemNode.parentNode.removeChild(itemNode);
+        }
+    });
 
 
+    //delete from done list if clicked
+    elements.doneList.addEventListener('click', function(event) {
+        var itemNode = event.target;
 
+        if(itemNode.tagName.toLowerCase() === 'li') {
+            itemNode.parentNode.removeChild(itemNode);
+        }
+    })
 
 
 
